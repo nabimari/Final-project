c7eb50aa6653361c12432712b1d01f0ae99ece6a
nabimari <65827756+nabimari@users.noreply.github.com>
2024-11-27 10:23:30 +0200

    Add test.js to testbranch

--- src/pages/test.js
+++ src/pages/test.js
@@ -5,6 +5,7 @@ const test = () => {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>PageName</h1>
       <h1>PageName</h1>
+      <h1>PageName</h1>
       <p>This page is under construction.</p>
       <h2>hello</h2>
     </div>
