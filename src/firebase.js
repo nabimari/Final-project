f5d9fffffd960a7e968c944bda42221999e1eb13
YodaAmer1 <Yodaamer2406@gmail.com>
2024-12-23 15:13:39 +0200

    Final LogIn Version

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from 'firebase/auth'
 //import { db } from "../firebase";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -22,4 +23,7 @@ const firebaseConfig = {
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+export {auth, db}
